85fdde8e2c3ef94f0d9b7c2c6da3fbb8aa495c96
meenakshi275 <79053084+meenakshi275@users.noreply.github.com>
2021-03-17 18:30:52 +0530

    Add files via upload

---
+++ pattern 14.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=6, i, j, sum = 0;
+  
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 1; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (i = n - 1; i >= 1; i--)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (j = 1; j <= i * 2 - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+© 2021 GitHub, In
